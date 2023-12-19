e3ae3f3d43649634eb510a4892c10de810fb75a9
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-12-19 08:45:05 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+document.title = "\u200E";
+async function ok() {
+var pjs = "PJ\'S ROOM!"
+var tx = ""
+for (let i=0; i < pjs.length; i++) {
+  tx += pjs[i]
+  await sleep(250);
+  document.title = "\u200E" + tx;
+}
+await sleep(250);
+document.title = "\u200E" + pjs;
+await sleep(pjs.length()*500);
+for (let i=pjs.length; i > -1; i--) {
+  tx = tx.slice(0, -1);
+  await sleep(250);
+  document.title = "\u200E" + tx;
+}
+}
+setInterval(ok, 10000);
+ok();
