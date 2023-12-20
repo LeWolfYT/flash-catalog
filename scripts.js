20fa00cea0dd4ddeb6e583f4e2f82e1ec9771483
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-12-20 09:21:59 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -23,26 +23,9 @@ for (let i=pjs.length; i > -1; i--) {
 setInterval(ok, pjs.length()*1000);
 ok();
 
-function setupRuffle() {
 const play = document.querySelector('#play');
 const game = document.querySelector('#game');
-var swfobject = {ua: {pv: {}}, hasFlashPlayerVersion() { return true }};
-
-window.RufflePlayer = window.RufflePlayer || {};
-        window.addEventListener("load", (event) => {
-            const ruffle = window.RufflePlayer.newest();
-            const player = ruffle.createPlayer();
-            const container = document.getElementById("container"); 
-            container.appendChild(player);
-            
-            player.style.width = "800px"; 
-            player.style.height = "600px";
-        });
 
 play.onclick = (event) => {
-  player.load({
-                url: "lewolfyt.github.io/flash-catalog/" + game.value, 
-                backgroundColor: "#000", 
-             });
-}
+  swfobject.embedSWF("lewolfyt.github.io/flash-catalog/games/"+game.value, "ruffle", 800, 600)
 }
