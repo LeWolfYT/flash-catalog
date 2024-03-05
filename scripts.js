569808fb07a663387996d69d77800c15def96b60
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2024-03-05 08:58:02 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,9 @@ function sleep(ms) {
 
 document.title = "\u200E";
 var pjs = "Flash games!"
+
 async function ok() {
+var pjs = "PJ\'S ROOM!"
 var tx = ""
 for (let i=0; i < pjs.length; i++) {
   tx += pjs[i]
@@ -13,17 +15,16 @@ for (let i=0; i < pjs.length; i++) {
 }
 await sleep(250);
 document.title = "\u200E" + pjs;
-await sleep(5000);
+await sleep(pjs.length()*500);
 for (let i=pjs.length; i > -1; i--) {
   tx = tx.slice(0, -1);
   await sleep(250);
   document.title = "\u200E" + tx;
 }
 }
-setInterval(ok, pjs.length()*1000);
-var swfobject = {};
-
 
 var play = document.querySelector('#play');
 var game = document.querySelector('#game');
+
+setInterval(ok, pjs.length()*1000);
 ok();
